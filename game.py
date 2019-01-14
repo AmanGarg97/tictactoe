28acaac9da680852efa40361ed726fb27a07597e
AmanGarg <amangarg34.ag@gmail.com>
2019-01-14 10:07:50 +0530

    added some more functions

--- game.py
+++ game.py
@@ -64,4 +64,49 @@ def win_check(board, mark):
             board[9] == mark and board[6] == mark and board[3] == mark or
             board[7] == mark and board[5] == mark and board[3] == mark or
             board[9] == mark and board[5] == mark and board[1] == mark)
+  
+  
+#random module to randomly decide which player goes first. 
+
+import random
+
+def choose_first():
+    flip.randomint(0 , 1)
+    
+    if(flip == 0):
+        return 'Player 1'
+    else:
+        return 'Player 2'
+      
+      
+#returns a boolean indicating whether a space on the board is freely available.
+
+def space_check(board, position):
+    return(board[position] == ' ')
+  
+
+#checks if the board is full and returns a boolean value
+
+def full_board_check(board):
+    for i in range(1,10):
+        if(space_check(board,i)):
+            return False
+    
+    return True
+  
+# asks for a player's next position (as a number 1-9) and then uses the function from step 6 to check if it's a free position. If it is, then return the position for later use.
+
+
+def player_choice(board):
+    position = 0
+    
+    while position not  in [1,2,3,4,5,6,7,8,9] and not space_check(board , position):
+        position = int(input('Choose a position : (1-9)'))
+    
+    return position
+  
+  
+#asks the player if they want to play again and returns a boolean True if they do want to play again.
+
+
             
